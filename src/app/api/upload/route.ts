88bd9dfe9cb34b641acd7d2dc5c0bd357090f5fc
John Costa <johncosta027@gmail.com>
2025-06-19 09:37:35 +0100

    refactor: not using supabase storage, using postgres directly
    chore: removing supabase

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { parse } from 'papaparse';
-import { db, supabase } from './db';
+import { db } from './db';
 import { csvsTable } from "./schema";
 
 export async function POST(request: Request) {
@@ -23,25 +23,14 @@ export async function POST(request: Request) {
      * However this is not the bottleneck (AI processing is), so I won't solve this problem just yet.
      */
     const parsedCsv = parse(content);
-
     if (parsedCsv.errors.length > 0) {
         return NextResponse.json({ error: "CSV could not be parsed without error" }, { status: 400 });
     }
 
-    /*
-     * The name should be unique, which is not at all a constraint right now.
-     * We should generate some ID before hand and substitute the uri with that.
-     * But for now I will leave this as it is.
-     */
-    const path = file.name;
-
-    const uploadedData = await supabase.storage.from('csvs').upload(path, file);
-    if (uploadedData.error == null) {
-        return NextResponse.json({ error: "Could not upload CSV to bucket" }, { status: 500 });
-    }
+    const fileContent = await file.text();
 
     // TODO: Handle SQL errors.
-    const insertedCsv = await db.insert(csvsTable).values({ name: file.name, uri: path }).returning({ insertedId: csvsTable.id });
+    const insertedCsv = await db.insert(csvsTable).values({ name: file.name, file: fileContent }).returning({ insertedId: csvsTable.id });
     const insertedCsvId = insertedCsv[0].insertedId;
 
     return NextResponse.json({ id: insertedCsvId }, { status: 201 });
--- src/app/api/upload/schema.ts
+++ src/app/api/upload/schema.ts
@@ -3,5 +3,14 @@ import { pgTable } from "drizzle-orm/pg-core";
 export const csvsTable = pgTable("csvs", (t) => ({
     id: t.uuid().primaryKey().defaultRandom(),
     name: t.text().notNull(),
-    uri: t.text().notNull(),
+
+    /* We can store the file in `TEXT` type because of TOAST
+     * @link https://www.postgresql.org/docs/current/storage-toast.html
+     *
+     * TLDR: If the value is larger than a regular DB page size (and other euristics),
+     * Postgres will take it out of the row and store it somewhere else, leaving a pointer
+     * behind. This means for files under 1GB (roughly), we can store them directly in the
+     * database, and avoid having a third system (like an S3 bucket).
+     */
+    file: t.text().notNull(),
 }));
