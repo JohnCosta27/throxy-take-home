cf9eb28d6bbeac1d1e1e5bf5d4e8df0bbf3296d1
John Costa <johncosta027@gmail.com>
2025-06-19 16:42:31 +0100

    feat: very rough filtering

--- src/app/table.tsx
+++ src/app/table.tsx
@@ -1,46 +1,90 @@
 "use client"
 
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { CsvRow, getFilteredData } from "./api/companies/route";
-import { useEffect, useState } from "react";
-
+import { useCallback, useEffect, useState } from "react";
 import {
     createColumnHelper,
     flexRender,
     getCoreRowModel,
     useReactTable,
 } from '@tanstack/react-table'
+import {
+    Table as CNTable,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
 const getCompanyData = async (searchParams: URLSearchParams) => {
+
     return fetch(`http://localhost:3000/api/companies?${searchParams}`).then(res => res.json());
 }
 
 const columnHelper = createColumnHelper<CsvRow>()
 
-const columns = [
-    columnHelper.accessor('companyName', {
-        header: "Company Name",
-        cell: info => {
-            return info.row.original.companyName ?? <span style={{ color: 'red' }}>{info.row.original.companyNameRaw}</span>
+const useCreateQueryString = () => {
+    const searchParams = useSearchParams();
+    const router = useRouter();
+
+    return useCallback(
+        (name: string, value: string | undefined) => {
+            const params = new URLSearchParams(searchParams.toString());
+            if (value) {
+                params.set(name, value);
+            } else {
+                params.delete(name);
+            }
+            const stringParams = params.toString();
+            router.push(`${window.location.pathname}?${stringParams}`);
         },
-    }),
-    columnHelper.accessor('domain', {
-        header: "Domain",
-        cell: info => info.row.original.domain ?? <span style={{ color: 'red' }}>{info.row.original.domainRaw}</span>,
-    }),
-    columnHelper.accessor('city', {
-        header: "City",
-        cell: info => info.row.original.city ?? <span style={{ color: 'red' }}>{info.row.original.cityRaw}</span>,
-    }),
-    columnHelper.accessor('country', {
-        header: "Country",
-        cell: info => info.row.original.country ?? <span style={{ color: 'red' }}>{info.row.original.countryRaw}</span>,
-    }),
-    columnHelper.accessor('employeeSize', {
-        header: "Employee Size",
-        cell: info => info.row.original.employeeSize ?? <span style={{ color: 'red' }}>{info.row.original.employeeSizeRaw}</span>,
-    }),
-]
+        [searchParams]
+    );
+}
+
+const TextFilter = () => {
+    const createQueryString = useCreateQueryString();
+
+    return (
+        <Popover>
+            <PopoverTrigger>Open</PopoverTrigger>
+            <PopoverContent>
+                <input type="text" onChange={e => createQueryString('domain', e.target.value)} />
+            </PopoverContent>
+        </Popover>
+    )
+}
+
+const DropdownFilter = ({ options }: { options: string[] }) => {
+    const createQueryString = useCreateQueryString();
+    const [value, setValue] = useState<string | undefined>(undefined);
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuRadioGroup value={value} onValueChange={(v) => {
+                    const valueOrUndefined = v === value ? undefined : v;
+
+                    setValue(valueOrUndefined);
+                    createQueryString("employee_size", valueOrUndefined);
+                }}>
+                    {options.map(o => (
+                        <DropdownMenuRadioItem key={o} value={o}>{o}</DropdownMenuRadioItem>
+                    ))}
+                </DropdownMenuRadioGroup>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
 
 export const Table = () => {
     const searchParams = useSearchParams();
@@ -48,6 +92,7 @@ export const Table = () => {
     const [loading, setLoading] = useState(false);
     const [data, setData] = useState<Awaited<ReturnType<typeof getFilteredData>> | undefined>(undefined);
 
+    // TODO: debounce this? It's going to hammer the server.
     useEffect(() => {
         setLoading(true);
         getCompanyData(searchParams).then((d) => {
@@ -56,49 +101,77 @@ export const Table = () => {
         })
     }, [searchParams]);
 
-    console.log(data);
+    const columns = [
+        columnHelper.accessor('companyName', {
+            header: "Company Name",
+            cell: info => {
+                return info.row.original.companyName ?? <span style={{ color: 'red' }}>{info.row.original.companyNameRaw}</span>
+            },
+        }),
+        columnHelper.accessor('domain', {
+            header: "Domain",
+            cell: info => info.row.original.domain ?? <span style={{ color: 'red' }}>{info.row.original.domainRaw}</span>,
+            meta: {
+                filterComponent: <TextFilter />
+            }
+        }),
+        columnHelper.accessor('city', {
+            header: "City",
+            cell: info => info.row.original.city ?? <span style={{ color: 'red' }}>{info.row.original.cityRaw}</span>,
+        }),
+        columnHelper.accessor('country', {
+            header: "Country",
+            cell: info => info.row.original.country ?? <span style={{ color: 'red' }}>{info.row.original.countryRaw}</span>,
+            meta: {
+                filterComponent: <TextFilter />
+            }
+        }),
+        columnHelper.accessor('employeeSize', {
+            header: "Employee Size",
+            cell: info => info.row.original.employeeSize ?? <span style={{ color: 'red' }}>{info.row.original.employeeSizeRaw}</span>,
+            meta: {
+                filterComponent: <DropdownFilter options={['1-10', '11-50', '51-200', '201-500', '501-1 000', '1 001-5 000', '5 001-10 000', '10 000+']} />
+            }
+        }),
+    ]
 
     const table = useReactTable({
         data: data ?? [],
         columns,
         getCoreRowModel: getCoreRowModel(),
+        manualFiltering: true,
     });
 
-    if (loading) {
-        return (
-            "Loading..."
-        )
-    }
-
     return (
-        <table>
-            <thead>
+        <CNTable>
+            <TableHeader>
                 {table.getHeaderGroups().map(headerGroup => (
-                    <tr key={headerGroup.id}>
-                        {headerGroup.headers.map(header => (
-                            <th key={header.id}>
-                                {header.isPlaceholder
-                                    ? null
-                                    : flexRender(
+                    <TableRow key={headerGroup.id}>
+                        {headerGroup.headers.map(header => {
+                            return (
+                                <TableHead key={header.id}>
+                                    {flexRender(
                                         header.column.columnDef.header,
                                         header.getContext()
                                     )}
-                            </th>
-                        ))}
-                    </tr>
+                                    {header.column.columnDef.meta?.filterComponent != null && header.column.columnDef.meta?.filterComponent}
+                                </TableHead>
+                            )
+                        })}
+                    </TableRow>
                 ))}
-            </thead>
-            <tbody>
+            </TableHeader>
+            <TableBody>
                 {table.getRowModel().rows.map(row => (
-                    <tr key={row.id}>
+                    <TableRow key={row.id}>
                         {row.getVisibleCells().map(cell => (
-                            <td key={cell.id}>
+                            <TableCell key={cell.id}>
                                 {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                            </td>
+                            </TableCell>
                         ))}
-                    </tr>
+                    </TableRow>
                 ))}
-            </tbody>
-        </table>
+            </TableBody>
+        </CNTable>
     )
 }
