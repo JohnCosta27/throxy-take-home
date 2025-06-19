5b8cc34568b6a2cb6c4e66d00f9e7f11f52a001d
John Costa <johncosta027@gmail.com>
2025-06-19 12:01:00 +0100

    feat: table to store CSV rows
    fix

--- src/app/api/upload/schema.ts
+++ src/app/api/upload/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable } from "drizzle-orm/pg-core";
+import { pgEnum, pgTable } from "drizzle-orm/pg-core";
 
 export const csvsTable = pgTable("csvs", (t) => ({
     id: t.uuid().primaryKey().defaultRandom(),
@@ -13,4 +13,37 @@ export const csvsTable = pgTable("csvs", (t) => ({
      * database, and avoid having a third system (like an S3 bucket).
      */
     file: t.text().notNull(),
+}));
+
+export const processingStatus = pgEnum('status', ['pending', 'processing', 'processed']);
+
+/* There is a world where I would have each of these attributes,
+ * be their own table instead of just text.
+ *
+ * This would make it better in a system where we want to filter for
+ * only a specific city, because we could use foreign keys to 
+ * uphold contraints, rename every and stuff like that.
+ *
+ * But for this simple system, I will leave it like this to avoid
+ * over-engineering it.
+ */
+export const csvRowsTable = pgTable("csvRows", (t) => ({
+    id: t.uuid().primaryKey().defaultRandom(),
+    csvId: t.uuid().references(() => csvsTable.id).notNull(),
+
+    companyNameRaw: t.text(),
+    companyName: t.text(),
+
+    cityRaw: t.text(),
+    city: t.text(),
+
+    countryRaw: t.text(),
+    country: t.text(),
+
+    // TODO: this doesnt feel right, since it's really a number.
+    // But I want to store buckets so I will leave this for now.
+    employeeSizeRaw: t.text(),
+    employeeSize: t.text(),
+
+    status: processingStatus().default('pending').notNull(),
 }));
