63cea7da1a657b29f1bc7474f3958e036cae7130
John Costa <johncosta027@gmail.com>
2025-06-20 11:41:57 +0100

    feat: adding processing before the AI

--- src/app/api/process/route.ts
+++ src/app/api/process/route.ts
@@ -6,6 +6,7 @@ import { generateObject } from 'ai';
 import { openai } from '@ai-sdk/openai';
 import { z } from "zod";
 import { CSV_COLUMNS } from "../upload/route";
+import { CsvRow } from "../companies/route";
 
 const DEFAULT_CHUNK_SIZE = 10;
 
@@ -27,6 +28,46 @@ const zip = <T, K>(arr1: T[], arr2: K[]) => {
     return arr1.map((x, i) => [x, arr2[i]] as const);
 }
 
+const maybeTrim = (s: string | null) => s?.trim() ?? null
+
+type ProcessingRecord<T> = {
+    [K in keyof T]: (field: T[K]) => T[K]
+}
+
+/*
+ * We can incrementally add fields and processing functions here.
+ * For API calls we could make this async so we're free to do anything we'd like here.
+ * I will keep it simple, and just do some basic regex + trimming
+ */
+const processingFields: Partial<ProcessingRecord<CsvRow>> = {
+    domainRaw: (domain) => domain?.replaceAll(" ", "") ?? null,
+    companyNameRaw: maybeTrim,
+    cityRaw: maybeTrim,
+    countryRaw: maybeTrim,
+}
+
+const process = (row: CsvRow): CsvRow => {
+    const copiedRow = structuredClone(row);
+    for (const [k, v] of Object.entries(copiedRow)) {
+        const processingFn = processingFields[k as keyof typeof processingFields];
+        if (processingFn == null || v == null) {
+            continue
+        }
+
+        /*
+         * Typescript always struggles with Object.entries functions
+         * It can't figure out what I'm trying to do here.
+         * SAFETY: We know that Object.entries will return the correct key-value,
+         * And we know `processingFn` is typed such that the right key corresponds to the
+         * right function.
+         */
+        //@ts-ignore
+        copiedRow[k] = processingFn(v);
+    }
+
+    return copiedRow;
+}
+
 const csvRowToText = (row: InferInsertModel<typeof csvRowsTable>): string => {
     return `${row.companyNameRaw ?? ''},${row.domainRaw ?? ''},${row.cityRaw ?? ''},${row.countryRaw ?? ''},${row.employeeSizeRaw ?? ''}`
 }
@@ -35,10 +76,11 @@ const enhanceAndClean = async (data: InferInsertModel<typeof csvRowsTable>[]) =>
     const { object } = await generateObject({
         model: openai('gpt-4o'),
         system: `
-            You are an agent that helps clean up CSV rows.
+            You are an agent that helps clean up and enhance CSV rows.
             You will be given a CSV file as input and your job is to clean it up and make the formats consistent.
 
-            Do not create extra rows, but do add information for rows which are missing information.
+            Do not create extra rows.
+            If a part of a row is empty, you can try and figure out what it should be, if you know it, keep it blank if you don't.
             
             You must preserve the order of the rows, and must always return the same number of rows as your input.
         `,
@@ -72,8 +114,8 @@ export async function POST(request: Request) {
     await db.update(csvRowsTable).set({ status: 'processing' }).where(eq(csvRowsTable.csvId, csvId));
 
     const csvRows = await db.select().from(csvRowsTable).where(eq(csvRowsTable.csvId, csvId));
-
-    const chunkedRows = chunkRows(DEFAULT_CHUNK_SIZE, csvRows);
+    const preprocessedRows = csvRows.map(process);
+    const chunkedRows = chunkRows(DEFAULT_CHUNK_SIZE, preprocessedRows);
 
     const enhancedRows = (await Promise.all(chunkedRows.map(enhanceAndClean))).flat();
 
